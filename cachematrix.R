31bed796333c2959e0dffa87b0123c1b9e28e6ba
Ernesto Reyes Zamorano <errezam@gmail.com>
2016-03-21 01:48:18 -0600

    primer intento

--- cachematrix.R
+++ cachematrix.R
@@ -5,11 +5,25 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  
+  esta <- function() x
+  inve <- NULL
+  guarda <- function(solve) inve <<- solve
+  busca <- function() inve
+  list(esta = esta, guarda = guarda, busca = busca)
 }
-
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inve <- x$busca()
+  if(!is.null(inve)) {
+    message("getting cached data")
+    return(inve)
+  }
+  data <- x$esta()
+  inve <- solve(data)
+  x$guarda(inve)
+  inve
 }
