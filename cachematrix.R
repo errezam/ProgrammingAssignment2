592789c29eb73fae7240772dfd405d20e34d82a2
Ernesto Reyes Zamorano <errezam@gmail.com>
2016-03-25 16:43:47 -0600

    Second submission

--- cachematrix.R
+++ cachematrix.R
@@ -4,19 +4,36 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+## This function creates another object with four new functions: 
+## set() stores a matrix.
+## esta() shows the matrix cached.
+## guarda() stores the inverse of the matrix.
+## busca() retrieves the inverse of the matrix if cached.
   
   esta <- function() x
   inve <- NULL
   guarda <- function(solve) inve <<- solve
   busca <- function() inve
   list(esta = esta, guarda = guarda, busca = busca)
+  
+  inve <- NULL
+  set <- function(y) {
+    esta <<- y
+    inve <<- NULL
+  }
+  esta <- function() esta
+  guarda <- function(solve) inve <<- solve
+  busca <- function() inve
+  list(set = set, esta = esta, guarda = guarda, busca = busca)
 }
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Check within and object made with makeCacheMatrix if an inverse has been
+## calculated if it has, returns the cached inverse matrix. If it hasn't 
+## calculates the inverse, stores and returns the result
+  
   inve <- x$busca()
   if(!is.null(inve)) {
     message("getting cached data")
